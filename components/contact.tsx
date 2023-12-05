be25abe3e1d9abe368e5ded0a1d1e1843f27a753
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2023-12-05 13:42:13 +0300

    first commit

--- components/contact.tsx
+++ components/contact.tsx
@@ -32,16 +32,43 @@ export default function Contact() {
       <SectionHeading>Contact me</SectionHeading>
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Feel free to reach out via email.
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:ali@alialfatlawi.me">
+          ali@alialfatlawi.me
+        </a>{" "}
+        or through this form.
       </p>
 
-      {/* Button to open email app */}
-      <a
-        href="mailto:ali@alialfatlawi.me"
-        className="mt-10 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg cursor-pointer hover:bg-blue-700 transition"
+      <form
+        className="mt-10 flex flex-col dark:text-black"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
       >
-        Send me an Email
-      </a>
+        <input
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
     </motion.section>
   );
 }
